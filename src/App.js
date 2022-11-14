567773fff16eaf24d4b9c056b6495e70837016f5
Anny Fiorella <annygutierrezlopez@gmail.com>
2022-11-13 19:28:17 -0500

    Add new paragraph - friend

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ function App() {
   return (
     <div style={{ width: '100vw', height: '100vh', display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
       <p style={{ fontWeight: 'bold', fontSize: '50px' }}>Hello</p>
+      <p style={{ fontWeight: 'bold', fontSize: '50px' }}>Friend</p>
     </div>
   );
 }
