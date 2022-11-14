d0c8cc18de90f6068fd4c0b73649d7ce684a80fc
Anny Fiorella <annygutierrezlopez@gmail.com>
2022-11-13 19:41:21 -0500

    feat(App): Add new button

--- src/App.js
+++ src/App.js
@@ -2,9 +2,10 @@ import './App.css';
 
 function App() {
   return (
-    <div style={{ width: '100vw', height: '100vh', display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
+    <div style={{ width: '100vw', height: '100vh', display: 'flex', justifyContent: 'center', alignItems: 'center', flexDirection: 'column' }}>
       <p style={{ fontWeight: 'bold', fontSize: '50px' }}>Hello</p>
       <p style={{ fontWeight: 'bold', fontSize: '50px' }}>Friend .</p>
+      <button style={{ backgroundColor: '#29EAD2', color: 'black', fontWeight: 'bold', fontSize: '20px' }}>Click me</button>
     </div>
   );
 }
